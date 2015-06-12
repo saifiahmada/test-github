8701fb0f911db6602b19a13cbc0d3bf1d06fc03e
saifiahmada <saifiahmada@gmail.com>
2015-06-12 16:13:44 +0800

    commit pertama github

---
+++ src/main/java/demo/TestGithubApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGithubApplication.class, args);
+    }
+}
